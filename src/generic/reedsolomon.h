103bb5c14e9efefd81193c325f9e18f0e256339b
Xerbo <xerbo@protonmail.com>
2021-05-10 11:35:19 +0100

    Clean up Reed Solomon implementation

--- src/generic/reedsolomon.h
+++ src/generic/reedsolomon.h
@@ -5,42 +5,34 @@
  *      Author: Lucas Teske
  */
 
-#ifndef INCLUDES_REEDSOLOMON_H_
-#define INCLUDES_REEDSOLOMON_H_
+#ifndef GENERIC_REEDSOLOMON_H
+#define GENERIC_REEDSOLOMON_H
 
 #include <cstdint>
 
 extern "C" {
-    #include <correct.h>
+#include <correct.h>
 }
 
 namespace SatHelper {
     class ReedSolomon {
-    private:
-        bool copyParityToOutput;
-        correct_reed_solomon *rs;
+        public:
+            ReedSolomon();
+            ~ReedSolomon();
 
-    public:
-        ReedSolomon();
+            // CCSDS standard (255,223) RS codec with dual-basis symbol representation
+            ssize_t decode_ccsds(uint8_t *data);
+            // CCSDS standard (255,223) RS codec with conventional (*not* dual-basis) symbol representation
+            ssize_t decode_rs8(uint8_t *data);
 
-        // CCSDS standard (255,223) RS codec with dual-basis symbol representation
-        uint32_t decode_ccsds(uint8_t *data);
+            // Deinterleave by I
+            void deinterleave(const uint8_t *data, uint8_t *output, uint8_t pos, uint8_t I);
+            // Interleave by I
+            void interleave(const uint8_t *data, uint8_t *output, uint8_t pos, uint8_t I);
 
-        // CCSDS standard (255,223) RS codec with conventional (*not* dual-basis) symbol representation
-        uint32_t decode_rs8(uint8_t *data);
-
-        // Deinterleave by I
-        void deinterleave(uint8_t *data, uint8_t *output, uint8_t pos, uint8_t I);
-
-        // Interleave by I
-        void interleave(uint8_t *data, uint8_t *output, uint8_t pos, uint8_t I);
-
-        inline void SetCopyParityToOutput(bool copyParityToOutput) {
-            this->copyParityToOutput = copyParityToOutput;
-        }
-
-        ~ReedSolomon();
+        private:
+            correct_reed_solomon *rs;
     };
 }
 
-#endif /* INCLUDES_REEDSOLOMON_H_ */
+#endif
