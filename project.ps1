d9fa4ebd861ba42d1d453b681421077bc993c5de
Xerbo <xerbo@protonmail.com>
2021-10-01 22:08:10 +0100

    Update projection scripts Overwrite output file and delete intermeditary files

--- project.ps1
+++ project.ps1
@@ -3,4 +3,6 @@ $gcps = $($(Get-Content $args[1]) -split " ")
 $temp = New-TemporaryFile
 
 gdal_translate $gcps $args[0] -of GTiff $temp
-gdalwarp -r bilinear -tps -t_srs "epsg:4326" $temp $args[2]
+gdalwarp -overwrite -r bilinear -tps -t_srs "epsg:4326" $temp $args[2]
+
+Remove-Item -Path $temp
--- project.sh
+++ project.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 # https://github.com/Xerbo/LeanHRPT-Decode/wiki/Projection
-temp = "$(mktemp)"
+temp="$(mktemp)"
 
 gdal_translate $(cat "$2") "$1" -of GTiff "$temp"
-gdalwarp -r bilinear -tps -t_srs "epsg:4326" "$temp" "$3"
+gdalwarp -overwrite -r bilinear -tps -t_srs "epsg:4326" "$temp" "$3"
+
+rm "$temp"
