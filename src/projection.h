ab5a37b6518008da6b68969373ffd073a0322e5a
Xerbo <xerbo@protonmail.com>
2022-06-02 15:40:42 +0100

    Fix MinGW build

--- src/projection.h
+++ src/projection.h
@@ -22,10 +22,10 @@
 #include "satinfo.h"
 #include "orbit.h"
 #include "geo/geodetic.h"
+#include "math.h"
 #include <utility>
 #include <cmath>
 #include <vector>
-#include "geo/geodetic.h"
 
 const double EARTH_RADIUS = 6371.0;
 const double EARTH_CIRCUMFRANCE = EARTH_RADIUS*2.0*M_PI;
