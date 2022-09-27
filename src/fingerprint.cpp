Xerbo/LeanHRPT-Decode

commit 86b1ea55b2b53b77effc2751ffe2d25bcd5e7d11

Meteor "CADU" reading support, close #26

src/fingerprint.cpp
@@ -11,7 +11,7 @@
 #include "protocol/repack.h"
 #include "protocol/reverse.h"
 
-// Finds the most common value of T
+/// Finds the most common value of T
 template <typename T>
 class Scoreboard {
    public:
@@ -54,6 +54,12 @@ std::tuple<SatID, FileType, Protocol> Fingerprint::file(std::string filename) {
         case FileType::CADU:
         case FileType::VCDU: {
             SatID id = fingerprint_ccsds(stream, filetype);
+            if (id == SatID::MeteorM2) {
+                id = fingerprint_meteor(stream, FileType::CADU);
+                file.close();
+                return {id, FileType::CADU, Protocol::MeteorHRPT};
+            }
+
             file.close();
             return {id, filetype, *ccsds_downlinks(id).begin()};
         }
@@ -105,6 +111,7 @@ std::tuple<SatID, FileType, Protocol> Fingerprint::file(std::string filename) {
 SatID Fingerprint::fingerprint_ccsds(std::istream &stream, FileType type) {
     Scoreboard<SatID> s;
     uint8_t frame[1024];
+    size_t n = 0;
 
     while (is_running && !stream.eof()) {
         if (type == FileType::VCDU) {
@@ -127,6 +134,9 @@ SatID Fingerprint::fingerprint_ccsds(std::istream &stream, FileType type) {
         }
         // clang-format on
 
+        if (n++ > 1000) {
+            return SatID::MeteorM2;
+        }
         if (s.max() != SatID::Unknown) return s.max();
     }
 
@@ -292,6 +302,9 @@ SatID Fingerprint::fingerprint_meteor(std::istream &stream, FileType type) {
             if (deframer.work(buffer.data(), frame.data(), 1024)) {
                 have_frame = true;
             }
+        } else if (type == FileType::CADU) {
+            stream.read((char *)frame.data(), 1024);
+            have_frame = true;
         }
 
         if (have_frame) {

commit 00b068937d15c99d019fb11a774924a4dde3a23b

Fix Meteor LRPT decoding Was broken in 86b1ea5

src/fingerprint.cpp
@@ -54,7 +54,7 @@ std::tuple<SatID, FileType, Protocol> Fingerprint::file(std::string filename) {
         case FileType::CADU:
         case FileType::VCDU: {
             SatID id = fingerprint_ccsds(stream, filetype);
-            if (id == SatID::MeteorM2) {
+            if (id == SatID::MeteorM22) {
                 id = fingerprint_meteor(stream, FileType::CADU);
                 file.close();
                 return {id, FileType::CADU, Protocol::MeteorHRPT};
@@ -135,7 +135,7 @@ SatID Fingerprint::fingerprint_ccsds(std::istream &stream, FileType type) {
         // clang-format on
 
         if (n++ > 1000) {
-            return SatID::MeteorM2;
+            return SatID::MeteorM22;
         }
         if (s.max() != SatID::Unknown) return s.max();
     }